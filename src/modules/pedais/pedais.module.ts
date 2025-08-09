d55b4546d5271d148c48915e9cd2be3b5c799e7c
lucasdevfull <lucassilva270805@gmail.com>
2025-08-09 16:58:13 -0300

    Atualizando o projeto

--- src/modules/pedais/pedais.module.ts
+++ src/modules/pedais/pedais.module.ts
@@ -4,11 +4,10 @@ import { Module } from '@nestjs/common'
 import { EnrollmentRepository } from '@repositories/enrollment.repository'
 import { PedaisRepository } from '@repositories/pedais.repository'
 import { PedaisService } from '@services/pedais.service'
-import { PrismaModule } from '@/infra/prisma/prisma.module'
 import { PedaisResolver } from './resolver/pedais.resolver'
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule],
   providers: [
     PedaisResolver,
     PedaisService,
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,8 @@ import { UserRepository } from '@repositories/user.repository'
 import { UsersResolver } from '@resolvers/users.resolver'
 import { PasswordService } from '@services/password/password.service'
 import { UsersService } from '@services/users.service'
-import { PrismaModule } from '@/infra/prisma/prisma.module'
 
 @Module({
-  imports: [PrismaModule],
   providers: [UsersResolver, UsersService, PasswordService, UserRepository],
   exports: [UsersService, UserRepository],
 })
