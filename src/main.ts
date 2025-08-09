251bd97aa595ad6d1e1c1afb099bee4ac4c2c47c
lucasdevfull <lucassilva270805@gmail.com>
2025-08-09 18:26:48 -0300

    Atualização do main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { NestExpressApplication } from '@nestjs/platform-express'
+import type { NestExpressApplication } from '@nestjs/platform-express'
 
 declare const module: any
 
